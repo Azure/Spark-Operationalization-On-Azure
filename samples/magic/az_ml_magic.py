4ac09fc3925ddd5f4bba964b44263060b37648ad
Steve Cobb <scobb1@utexas.edu>
2017-04-05 10:53:46 -0700

    Hacked the profile logger to be able to display in Jupyter.

--- samples/magic/az_ml_magic.py
+++ samples/magic/az_ml_magic.py
@@ -2,13 +2,12 @@ from __future__ import print_function
 from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 from IPython.utils.ipstruct import Struct
 
+
 @magics_class
 class AMLHelpers(Magics):
-
-
     @cell_magic
     def save_file(self, parameter_s='', cell=None):
-        opts, arg_str = self.parse_options(parameter_s, 'f:', list_all = True, posix = False)
+        opts, arg_str = self.parse_options(parameter_s, 'f:', list_all=True, posix=False)
         if cell is not None:
             arg_str += '\n' + cell
 
@@ -18,24 +17,31 @@ class AMLHelpers(Magics):
     def list_subs(self, line, cell):
         from azure.cli.core._profile import Profile
         from azure.cli.core._util import CLIError
+        from azure.cli.core.azlogging import CustomStreamHandler
         import logging
         class PrintLogger(logging.Logger):
-            def _log(self, level, msg, args, exc_info=None, extra=None, stack_info=False):
+            def _log(self, level, msg, args, exc_info=None, extra=None,
+                     stack_info=False):
                 print('{}: {}'.format(level, msg).format(args))
+
         logging.setLoggerClass(PrintLogger)
+        profile_logger = logging.getLogger('az.azure.cli.core._profile')
+        profile_logger.addHandler(CustomStreamHandler(logging.DEBUG, {
+            True: '%(message)s',
+            False: '%(levelname)s: %(message)s',
+        }))
         profile = Profile()
         try:
             profile.get_subscription()
         except CLIError as exc:
             profile.find_subscriptions_on_login(True, None, None, None, None)
-        subs = p.load_cached_subscriptions()
+        subs = profile.load_cached_subscriptions()
         if not subs:
             print('No subscriptions available.')
             print('Please run `az login` from the console then try again')
             return
         print("Available subscriptions:\n  {}".format('\n  '.join(
-            [sub['name'] for sub in p.load_cached_subscriptions()])))
-
+            [sub['name'] for sub in subs])))
 
     @cell_magic
     def env_setup(self, line, cell):
@@ -61,12 +67,12 @@ class AMLHelpers(Magics):
     def _save_file(code, opts, namespace):
         # read arguments
         opts.merge(Struct(f=None))
-        file_name=opts.f
+        file_name = opts.f
 
         if not file_name:
             return "Usage: %%save_file -f file_name"
 
-        file_name=file_name[0]
+        file_name = file_name[0]
 
         with open(file_name, 'w') as fileName:
             fileName.write(code)
@@ -76,4 +82,5 @@ class AMLHelpers(Magics):
 
 
 from IPython import get_ipython
+
 get_ipython().register_magics(AMLHelpers)
